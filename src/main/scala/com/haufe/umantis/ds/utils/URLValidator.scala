7b8fe7385f106a2eb2b586af8fb3e31e96e01ae4
redsk <nicola.bova@gmail.com>
2018-09-07 11:30:47 +0200

    URLValidator changed to mappartitions

--- src/main/scala/com/haufe/umantis/ds/utils/URLValidator.scala
+++ src/main/scala/com/haufe/umantis/ds/utils/URLValidator.scala
@@ -1,14 +1,17 @@
 package com.haufe.umantis.ds.utils
 
-import org.apache.spark.ml.UnaryTransformer
+import com.haufe.umantis.ds.nlp.params.ValidateColumnSchema
+import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
 import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.sql.catalyst.ScalaReflection.schemaFor
-import org.apache.spark.sql.types.DataType
+import org.apache.spark.sql.types.{StructField, StructType}
 
 
 class URLValidator(override val uid: String)
-// first arg is input, second output, third is class name
-  extends UnaryTransformer[Seq[String], Seq[String], URLValidator] {
+  extends Transformer with HasInputCol with HasOutputCol with ValidateColumnSchema {
 
   def this() = this(Identifiable.randomUID("URLValidator"))
 
@@ -16,14 +19,37 @@ class URLValidator(override val uid: String)
     urls.filter(url => URLIdentifier.isValid(url.toLowerCase))
   }
 
-  override protected def createTransformFunc: Seq[String] => Seq[String] = validateURLs
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    val result = dataset.toDF().rdd.mapPartitions(iter => {
+      iter.map(r => {
+        val urls = r.getAs[Seq[String]]($(inputCol))
+        val validatedUrls = urls.filter(url => URLIdentifier.isValid(url.toLowerCase))
 
+        Row.fromSeq(r.toSeq :+ validatedUrls)
+      })
+    })
 
-  override protected def validateInputType(inputType: DataType): Unit = {
-    require(inputType == schemaFor[Seq[String]].dataType)
+    dataset.sparkSession.createDataFrame(result, transformSchema(dataset.schema))
   }
 
-  override protected def outputDataType: DataType = {
-    schemaFor[Seq[String]].dataType
+    /** @group setParam */
+  def setInputCol(value: String): this.type = set(inputCol, value)
+
+  /** @group setParam */
+  def setOutputCol(value: String): this.type = set(outputCol, value)
+
+
+  override def transformSchema(schema: StructType): StructType = {
+    validateColumnSchema($(inputCol), schemaFor[Seq[String]].dataType, schema)
+
+    if (schema.fieldNames.contains($(outputCol))) {
+      throw new IllegalArgumentException(s"Output column ${$(outputCol)} already exists.")
+    }
+    val outputFields = schema.fields :+
+      StructField($(outputCol), schemaFor[Seq[String]].dataType, nullable = false)
+
+    StructType(outputFields)
   }
+
+  override def copy(extra: ParamMap): URLValidator = defaultCopy[URLValidator](extra)
 }
