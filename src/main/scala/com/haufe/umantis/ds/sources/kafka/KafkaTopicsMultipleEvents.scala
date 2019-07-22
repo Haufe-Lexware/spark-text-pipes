306b193d3b56efe1e1a5e195eeafeeca2df4f096
redsk <nicola.bova@gmail.com>
2019-07-22 17:46:59 +0200

    added spark sql streaming + hdfs

--- src/main/scala/com/haufe/umantis/ds/sources/kafka/KafkaTopicsMultipleEvents.scala
+++ src/main/scala/com/haufe/umantis/ds/sources/kafka/KafkaTopicsMultipleEvents.scala
@@ -1,20 +1,42 @@
 package com.haufe.umantis.ds.sources.kafka
 
 import java.nio.ByteBuffer
+import java.util.UUID
 
 import com.haufe.umantis.ds.sources.kafka.serde.{DataFrameAvroHelpers, SchemaRegistryHelper}
 import com.haufe.umantis.ds.spark.{DataFrameHelpers, SparkIO}
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.streaming.{DataStreamWriter, StreamingQuery, StreamingQueryListener, Trigger}
+import org.apache.spark.sql.streaming.StreamingQueryListener.{QueryProgressEvent, QueryStartedEvent, QueryTerminatedEvent}
+import org.apache.hadoop.fs.Path
+import org.apache.spark.storage.StorageLevel
+
+import scala.collection.mutable
 
 case class EventMetadata(latestVersion: Int, latestSchema: String, schemaIDs: List[Int])
+
+case class KafkaHdfsBridge(
+                            topic: String,
+                            event: String,
+                            filePath: String,
+                            checkpointFilePath: String,
+                            hadoopPath: String,
+                            fileFormat: String,
+                            dataStreamWriter: DataStreamWriter[Row],
+                            var streamingQuery: StreamingQuery
+                          ) {
+  val copyFilename = s"$filePath-COPY"
+}
 
 
 class KafkaTopicsMultipleEvents(
                                  val schemaRegistryURL: String,
-                                 val kafkaURL: String
+                                 val kafkaURL: String,
+                                 val hdfsURL: String,
+                                 val hdfsBase: String
                                )
   extends SparkIO
     with DataFrameHelpers with DataFrameAvroHelpers {
@@ -22,6 +44,8 @@ class KafkaTopicsMultipleEvents(
   import currentSparkSession.implicits._
 
   import scala.collection.JavaConverters._
+
+  installListeners()
 
   type Topic = String
   type Event = String
@@ -116,5 +140,138 @@ class KafkaTopicsMultipleEvents(
     if (rawDf.isEmpty) raw.unpersist()
 
     dfs
+  }
+
+  def getSource(topic: String): DataFrame = {
+    currentSparkSession
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafkaURL)
+      .option("startingOffsets", "earliest")
+      .option("subscribe", topic)
+      .load()
+      .filter("value is not null")
+      .withColumn("schemaID", schemaID(col("value")))
+      .select("schemaID", "value")
+  }
+
+  val queries: mutable.Map[Topic, Map[Event, KafkaHdfsBridge]] = mutable.Map()
+
+  def create(
+              topic: String,
+              intervalMs: Int = 0,
+              fileFormat: String = "parquet"
+            )
+  : Unit = {
+
+    val source = getSource(topic)
+
+    queries(topic) = schemas(topic)
+      .map { case (event, metadata: EventMetadata) =>
+        val eventName = event.split('.').last
+
+        val hadoopPath = s"$hdfsBase/$topic/$eventName"
+        val filePath = s"$hdfsURL/$hadoopPath"
+        val checkpointFilePath = s"$filePath-CHECKPOINT"
+
+        val dataStreamWriter = source
+          .filter($"schemaID".isin(metadata.schemaIDs: _*))
+          .from_confluent_avro("value", "value", metadata.latestSchema)
+          .select("value.*")
+          .writeStream
+          .outputMode("append")
+          .option("checkpointLocation", checkpointFilePath)
+          .format(fileFormat)
+          .trigger(Trigger.ProcessingTime(intervalMs))
+
+        val query = dataStreamWriter
+          .start(filePath)
+
+        eventName -> KafkaHdfsBridge(
+          topic = topic,
+          event = eventName,
+          filePath = filePath,
+          checkpointFilePath = checkpointFilePath,
+          hadoopPath = hadoopPath,
+          fileFormat = fileFormat,
+          dataStreamWriter = dataStreamWriter,
+          streamingQuery = query
+        )
+      }
+  }
+
+  val queriesToStop: mutable.Set[UUID] = mutable.Set()
+
+  def stopAndAwaitQueriesTermination(queries: Iterable[StreamingQuery]): Unit = {
+    val queryIds = queries.map(_.id)
+    queriesToStop ++= queryIds
+    queries.foreach(_.awaitTermination())
+    queriesToStop --= queryIds
+  }
+
+  def stop(): Unit = {
+    stopAndAwaitQueriesTermination(queries.values.flatMap(_.values.map(_.streamingQuery)))
+  }
+
+  def stop(topic: Topic): Unit = {
+    stopAndAwaitQueriesTermination(queries(topic).values.map(_.streamingQuery))
+  }
+
+  def stop(topic: Topic, event: Event): Unit = {
+    stopAndAwaitQueriesTermination(Iterable(queries(topic)(event).streamingQuery))
+  }
+
+  def start(topic: Topic, event: Event): Unit = {
+    val bridge = queries(topic)(event)
+    if (!bridge.streamingQuery.isActive) {
+      bridge.streamingQuery = bridge.dataStreamWriter.start(bridge.filePath)
+    }
+  }
+
+  def start(topic: Topic): Unit = {
+    queries(topic).keys.foreach(start(topic, _))
+  }
+
+  def start(): Unit = {
+    queries.keys.foreach(start)
+  }
+
+  def get(topic: Topic, event: Event): DataFrame = {
+    stop(topic, event)
+    val bridge = queries(topic)(event)
+    val df = currentSparkSession
+      .read
+      .format(bridge.fileFormat)
+      .load(bridge.filePath)
+      .persist(StorageLevel.MEMORY_AND_DISK)
+    start(topic, event)
+    df
+  }
+
+  def createSnapshot(topic: Topic, event: Event): Path = {
+    val bridge = queries(topic)(event)
+    val conf = currentSparkSession.sparkContext.hadoopConfiguration
+    val fs = org.apache.hadoop.fs.FileSystem.get(conf)
+    fs.createSnapshot(new Path(s"/${bridge.filePath}"))
+  }
+
+  def installListeners(): Unit = {
+    currentSparkSession.streams.addListener(new StreamingQueryListener() {
+      override def onQueryStarted(queryStarted: QueryStartedEvent): Unit = {
+        println("Query started: " + queryStarted.id)
+      }
+
+      override def onQueryTerminated(queryTerminated: QueryTerminatedEvent): Unit = {
+        println("Query terminated: " + queryTerminated.id)
+      }
+
+      override def onQueryProgress(queryProgress: QueryProgressEvent): Unit = {
+        //        println("Query made progress: " + queryProgress.progress)
+        //        println("Query made progress: " + queryProgress.progress.id)
+        if (queriesToStop.contains(queryProgress.progress.id)) {
+          currentSparkSession.streams.get(queryProgress.progress.id).stop()
+        }
+      }
+    })
   }
 }
